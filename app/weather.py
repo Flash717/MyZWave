8a539172d8fca50753934af64fdaff16d0f69960
Florian Knaus <florian.knaus@gmail.com>
2022-02-02 11:49:29 -0600

    updated .gitignore and weather.py

--- app/weather.py
+++ app/weather.py
@@ -1,17 +1,40 @@
 import requests
 import os
-import xml.etree.ElementTree as ET
-from dateutil import parser
+from datetime import datetime
 
-url = 'http://api.openweathermap.org/data/2.5/forecast?{type}={id}&APPID={apikey}&mode=xml'
+url = 'http://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly&APPID={apikey}'
 api_key = os.environ['OPENWEATHER_API_KEY']
 
+def d_of_t(timestamp):
+    """Helper function to convert timestamp to datetime
+    :param timestamp: incoming timestamp
+    :return datetime: converted datetime value
+    """
+    try:
+        return datetime.fromtimestamp(timestamp)
+    except (Exception):
+        return timestamp
 
-def get_sun(id, type='q'):
-    response = requests.get(url.format(type=type, id=id, apikey=api_key))
+def get_next_sun(lat, lon):
+    """Get next sunrise and sunset for provided latitude and longitude.
+
+    :param lat: latitude float
+    :param lon: longitude float
+    :return sunrise, sunset: returns timestamp for next sunrise and sunset
+    """
+    response = requests.get(url.format(lat=lat, lon=lon, apikey=api_key))
     if response.ok:
-        tree = ET.fromstring(response.content)
-        sun = tree.find('sun')
-        if sun is not None:
-            return parser.parse(sun.attrib.get('rise')), parser.parse(sun.attrib.get('set'))
+        body = response.json()
+        sunrise = body['daily'][0]['sunrise']
+        if d_of_t(sunrise) < datetime.now():
+            sunrise = body['daily'][1]['sunrise']
+        sunset = body['daily'][0]['sunset']
+        if d_of_t(sunset) < datetime.now():
+            sunset = body['daily'][1]['sunset']
+        return sunrise, sunset
     return None, None
+
+if __name__ == '__main__':
+    sunrise, sunset = get_next_sun(32.18,-96.81)
+    print(d_of_t(sunrise))
+    print(d_of_t(sunset))
